a63810d40b76574ffc43dfe26a86c5ece9e3d12b
satyendra2 <santyrocker88@gmail.com>
2021-12-04 17:30:33 +0000

    added advikalogs

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -20,6 +20,7 @@ public class App {
         Logger log = LoggerFactory.getLogger(App.class);
         log.info("Java app started");
         log.info("hello satya");
+        log.info("hello advika");
     }
 
     public String getStatus() {
